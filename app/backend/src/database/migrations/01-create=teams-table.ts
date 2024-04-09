1d44d18a8872073957bc0a3809281e68e19a6a53
andrew-marquezin <andrewmmarinho@gmail.com>
2024-04-08 23:31:50 -0300

    commit bomba #1, configurando docker e requisito 1

---
+++ app/backend/src/database/migrations/01-create=teams-table.ts
@@ -0,0 +1,22 @@
+import { DataTypes, Model, QueryInterface } from "sequelize";
+import ITeam from "../../Interfaces/teams/ITeam";
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<ITeam>>('teams', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      teamName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    });
+  },
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('teams');
+  },
+};
